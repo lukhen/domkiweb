a32f51b93b33ef9ecea3f3a5e8124007beed27fe
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-12 16:03:10 +0200

    Update Description2.tsx

--- islands/Description2.tsx
+++ islands/Description2.tsx
@@ -1,108 +1,77 @@
 export default function Description2() {
   return (
       
-      <div class="container p-6 px-6 mx-auto bg-white dark:bg-gray-800">
-	  <div class="mb-16 text-center">
-              <h2 class="text-base font-semibold tracking-wide text-indigo-600 uppercase">
-		  Features
-              </h2>
-              <p class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-		  Kilka faktów
-              </p>
-	  </div>
-	  <div class="flex flex-wrap my-12 dark:text-white">
-              <div class="w-full p-8 border-b md:w-1/2 md:border-r lg:w-1/3">
-		  <div class="flex items-center mb-6">
-                      <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-			  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-			  </path>
-                      </svg>
-                      <div class="ml-4 text-xl">
-			  Lokalizacja
-                      </div>
-		  </div>
-		  <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
-                      Nasze miejsce jest po prostu idealne:
+     <div class="container p-6 px-6 mx-auto bg-white dark:bg-gray-800">
+   <div class="mb-16 text-center">
+         <h2 class="text-base font-semibold tracking-wide text-indigo-600 uppercase">
+        Nasza Oferta
+         </h2>
+         <p class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+        Czas na relaks i zabawę!
+         </p>
+   </div>
+   <div class="flex flex-wrap my-12 dark:text-white">
+
+       <div class="w-full p-8 border-b md:w-1/2 md:border-r lg:w-1/3">
+         <div class="flex items-center mb-6">
+               <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                 </path>
+               </svg>
+               <div class="ml-4 text-xl">
+                  Lokalizacja
+               </div>
+         </div>
+         <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+              Nasze miejsce jest po prostu idealne:
 - Zaledwie **350 m** od morza – gotowy na plażowe szaleństwo?
 - Blisko centrum miejscowości – wszystkie atrakcje na wyciągnięcie ręki.
 - Z dala od miejskiego zgiełku – tu królują spokój i natura.
 - **1 km od jeziora** i centrum sportów wodnych – czas na aktywny wypoczynek!
 - **400 m od marketu** – wszystko, czego potrzebujesz, w zasięgu ręki.
+         </p>
+       </div>
 
-		  </p>
-              </div>
-              <div class="w-full p-8 border-b md:w-1/2 lg:w-1/3 lg:border-r">
-		  <div class="flex items-center mb-6">
-                      <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-			  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-			  </path>
-                      </svg>
-                      <div class="ml-4 text-xl">
-			  Dla dzieci
-                      </div>
-		  </div>
-		  <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
-		      Rodziny z dziećmi nie będą zawiedzione. Szybkie i wygodne dojście na plażę to rzecz najważniejsza. Jednak nie samą plażą turysta żyje. Duży, drewniany plac zabaw, rowery i piłkarzyki nie pozwolą się nudzić również po plaży lub w dni nieplażowe.
-		  </p>
-              </div>
-              <div class="w-full p-8 border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0">
-		  <div class="flex items-center mb-6">
-                      <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-			  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-			  </path>
-                      </svg>
-                      <div class="ml-4 text-xl">
-			  Bezpieczeństwo
-                      </div>
-		  </div>
-		  <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
-		      Teren jest ogrodzony i monitorowany całodobowo. Podgląd na plac zabaw dostępny w każdym tv.
-		  </p>
-              </div>
-              <div class="w-full p-8 border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0">
-		  <div class="flex items-center mb-6">
-                      <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-			  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-			  </path>
-                      </svg>
-                      <div class="ml-4 text-xl">
-			  Duże tarasy
-                      </div>
-		  </div>
-		  <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
-		      Obszerne drewniane terasy posłużą na wieczornego grilla lub jako platforma do opalania.
-		  </p>
-              </div>
-              <div class="w-full p-8 border-b md:w-1/2 md:border-r md:border-b-0 lg:w-1/3 lg:border-b-0">
-		  <div class="flex items-center mb-6">
-                      <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-			  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-			  </path>
-                      </svg>
-                      <div class="ml-4 text-xl">
-			  Parkowanie
-                      </div>
-		  </div>
-		  <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
-		      Niemal wszystkie domki mają możliwość parkowania przy domku.
-		  </p>
-              </div>
-              <div class="w-full p-8 md:w-1/2 lg:w-1/3">
-		  <div class="flex items-center mb-6">
-                      <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-			  <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-			  </path>
-                      </svg>
-                      <div class="ml-4 text-xl">
-			  Zwierzęta
-                      </div>
-		  </div>
-		  <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
-		      Zwierzęta są u nas mile widziane. Tuż za naszym ośrodkiem znajduje się dużo wolnej przestrzeni, gdzie bezstresowo można wyprowadzić swojego pupila.
-		  </p>
-              </div>
-	  </div>
-      </div>
+       <div class="w-full p-8 border-b md:w-1/2 lg:w-1/3 lg:border-r">
+         <div class="flex items-center mb-6">
+               <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                 </path>
+               </svg>
+               <div class="ml-4 text-xl">
+                  Dla dzieci
+               </div>
+         </div>
+         <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+              Szukasz atrakcji dla maluchów? Mamy to!
+- **Plac zabaw zewnętrzny** – miejsce na radość i śmiech na świeżym powietrzu.
+- **Sala zabaw** – bezpieczna przestrzeń do kreatywnej zabawy.
+         </p>
+       </div>
+
+       <div class="w-full p-8 border-b md:w-1/2 md:border-r lg:w-1/3 lg:border-r-0">
+         <div class="flex items-center mb-6">
+               <svg width="20" height="20" fill="currentColor" class="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
+                 </path>
+               </svg>
+               <div class="ml-4 text-xl">
+                  Rekreacja i relaks
+               </div>
+         </div>
+         <p class="leading-loose text-gray-500 dark:text-gray-200 text-md">
+              Czas na odprężenie! Oto, co znajdziesz u nas:
+- **Jacuzzi i sauna** – idealne na relaks po dniu pełnym przygód.
+- **Wypożyczalnia rowerów** – odkrywaj piękno okolicy na dwóch kółkach.
+- **Stół do piłkarzyków** oraz **XBOX** – świetna zabawa dla całej rodziny.
+- **Gry planszowe** i **biblioteczka** – dla miłośników spokojnych wieczorów z książką.
+- **Duże tarasy** – idealne na grilla, opalanie i chwile w towarzystwie bliskich.
+- **Drink bar** – orzeźwiające napoje, które umilą Twój pobyt.
+         </p>
+       </div>
+
+   </div>
+</div> 
 
   );
 }
