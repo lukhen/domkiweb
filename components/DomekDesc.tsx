be1874c2452323d1e4c2de4eda5b11fe599f5126
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-13 12:59:32 +0200

    Update DomekDesc.tsx

--- components/DomekDesc.tsx
+++ components/DomekDesc.tsx
@@ -13,38 +13,39 @@ interface Props {
 }
 
 
-const DomekDesc: preact.FunctionalComponent<Props> = ({id, title, images, elements}) => {
+const DomekDesc: preact.FunctionalComponent<Props> = ({ id, title, images, elements }) => {
     return (
-	<div id={id} class="lg:w-4/5 my-10  rounded-xl mx-auto  p-4 bg-white dark:bg-gray-800">
-	    <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+        <div id={id} class="lg:w-4/5 my-10 rounded-xl mx-auto p-4 bg-white dark:bg-gray-800">
+            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
 
-	    <div class="bg-white lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-12 lg:items-center">
-		<div class="lg:col-start-2 md:pl-20">
-		    <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:leading-9">
-			{title}
-		    </h4>
-		    <ul class="mt-2 ml-6">
-			{elements.map(el => (
-			    <li>
+            <div class="bg-white lg:grid lg:grid-cols-2 lg:gap-16 lg:items-center">
+                
+                {/* Tekst po prawej stronie */}
+                <div class="lg:col-start-2 md:pl-12 lg:pl-0 lg:pr-8 max-w-prose mx-auto">
+                    <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:leading-9">
+                        {title}
+                    </h4>
+                    <ul class="mt-4 ml-4 space-y-4">
+                        {elements.map(el => (
+                            <li>
+                                <h5 class="text-lg font-bold text-gray-900 dark:text-white">
+                                    {el.title}
+                                </h5>
+                                <p class="mt-1 text-base text-gray-500 dark:text-gray-300">
+                                    {el.text}
+                                </p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-					<h5 class="text-lg font-bold leading-6 text-gray-900 dark:text-white">
-					    {el.title}
-					</h5>
-					<p class="mt-2 text-base leading-6 text-gray-500 dark:text-gray-300">
-					    {el.text}
-					</p>
-
-			    </li>
-
-			))}
-		    </ul>
-		</div>
-		<div class="mt-10 -mx-4 md:-mx-12 lg:mt-0 lg:col-start-1">
-		    <Carousel items={images} />
-		</div>
-			
-	    </div>
-	</div>
+                {/* Karuzela po lewej stronie */}
+                <div class="mt-10 -mx-4 md:-mx-12 lg:mt-0 lg:col-start-1">
+                    <Carousel items={images} />
+                </div>
+            </div>
+        </div>
     );
 };
+
 export default DomekDesc
