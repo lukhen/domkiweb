4b69dc79f1deb0d8dc397b4856b214a6d2fb33c7
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-13 12:36:20 +0200

    Update DomekDesc.tsx

--- components/DomekDesc.tsx
+++ components/DomekDesc.tsx
@@ -23,11 +23,11 @@ const DomekDesc: preact.FunctionalComponent<Props> = ({id, title, images, elemen
 		    <h4 class="text-2xl font-extrabold leading-8 tracking-tight text-gray-900 dark:text-white sm:leading-9">
 			{title}
 		    </h4>
-		    <ul class="list-disc ml-6 mt-2>
+		    <ul class="mt-10">
 			{elements.map(el => (
-			    <li>
+			    <li class="mt-10">
 				<div class="flex">
-				     <div class="flex-shrink-0">
+				    <div class="flex-shrink-0">
 					<div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
 					    <svg width="10" height="10" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
 						<path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
