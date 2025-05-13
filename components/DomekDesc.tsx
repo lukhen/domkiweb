f41aa69e5ac90f61baf5a2af136f60f77bc58361
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-13 12:38:23 +0200

    Update DomekDesc.tsx

--- components/DomekDesc.tsx
+++ components/DomekDesc.tsx
@@ -29,10 +29,7 @@ const DomekDesc: preact.FunctionalComponent<Props> = ({id, title, images, elemen
 				<div class="flex">
 				    <div class="flex-shrink-0">
 					<div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
-					    <svg width="10" height="10" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-						<path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-						</path>
-					    </svg>
+					   
 					</div>
 				    </div>
 				    <div class="ml-4">
