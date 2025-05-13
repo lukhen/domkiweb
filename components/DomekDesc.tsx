0a00b4a214afa2231da37fca4dbff86337b232c0
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-13 13:31:30 +0200

    Update DomekDesc.tsx

--- components/DomekDesc.tsx
+++ components/DomekDesc.tsx
@@ -15,22 +15,26 @@ interface Props {
 
 const DomekDesc: preact.FunctionalComponent<Props> = ({ id, title, images, elements }) => {
     return (
-        <div id={id} class="lg:w-4/5 my-10 rounded-xl mx-auto p-4 bg-white dark:bg-gray-800">
+        <div id={id} class="lg:w-4/5 my-10 rounded-xl mx-auto p-4 bg-white dark:bg-gray-800 shadow-md">
             <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
 
             <div class="lg:grid lg:grid-cols-2 lg:gap-12 lg:items-start flex flex-col-reverse">
-                
-                {/* Tekst – przewijany tylko na dużych ekranach */}
-                <div class="md:pl-12 lg:max-h-[500px] lg:overflow-y-auto lg:pr-2">
-                    <h4 class="text-xl font-bold leading-6 tracking-tight text-gray-900 dark:text-white sm:leading-7">
+
+                {/* Tekst – scroll tylko na dużych ekranach */}
+                <div class="bg-gray-50 dark:bg-gray-700 p-4 rounded-lg md:pl-12 lg:max-h-[500px] lg:overflow-y-auto lg:pr-6 lg:border-r lg:border-gray-200 dark:lg:border-gray-700">
+                    <h4 class="text-xl font-bold leading-6 tracking-tight text-gray-900 dark:text-white sm:leading-7 mb-4">
                         {title}
                     </h4>
-                    <ul class="mt-4 ml-6 space-y-5">
+                    <ul class="mt-2 ml-2 space-y-6">
                         {elements.map(el => (
                             <li class="flex items-start space-x-3">
-                                {/* Ikona */}
-                                <svg class="flex-shrink-0 w-5 h-5 text-blue-500 mt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2l4-4m6 2a10 10 0 11-20 0a10 10 0 0120 0z" />
+                                {/* Ikona z Heroicons */}
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5" stroke="currentColor"
+                                     class="w-5 h-5 text-blue-500 mt-1">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M11.25 9.75h.008v.008H11.25V9.75zm0 3.75h.008v.008H11.25v-.008zm.75-7.5a8.25 8.25 0 110 16.5 8.25 8.25 0 010-16.5z" />
                                 </svg>
 
                                 {/* Tekst */}
@@ -55,6 +59,7 @@ const DomekDesc: preact.FunctionalComponent<Props> = ({ id, title, images, eleme
         </div>
     );
 };
+
 
 
 export default DomekDesc
