e92770a60ba679f710bf1d88269f3b57795d585d
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-14 14:13:20 +0200

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,7 +18,21 @@ export default function Home() {
 	
     return (
 	<>
-
+<Head>
+        <title>Domki Słoneczko</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QZ651WN12Y"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QZ651WN12Y');
+            `,
+          }}
+        />
+      </Head>
 	    <Menu />
 	    <LandingPage />
 	    <CardWith4Images
