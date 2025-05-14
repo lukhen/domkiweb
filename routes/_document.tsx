7aa19ebb03d7d2045381a740f20f7e7e53ea7b4b
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-14 12:48:09 +0200

    Update _document.tsx

--- routes/_document.tsx
+++ routes/_document.tsx
@@ -1,1 +1,7 @@
-
+<body>
+  <Main />
+  <Scripts />
+  <div style="position: fixed; bottom: 10px; left: 10px; background: red; color: white; padding: 4px; z-index: 9999;">
+    DOCUMENT LOADED
+  </div>
+</body>
