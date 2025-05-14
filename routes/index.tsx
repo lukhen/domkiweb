106271b62930e30ee1e3f9178ae5cb761d5e3035
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-14 11:18:11 +0200

    Update index.tsx, google analytics

--- routes/index.tsx
+++ routes/index.tsx
@@ -17,24 +17,35 @@ export default function Home() {
 	<>
 	    <Head>
 		<title>Domki Słoneczko</title>
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-QZ651WN12Y"></script>
+		<script
+		    dangerouslySetInnerHTML={{
+			__html: `
+			  window.dataLayer = window.dataLayer || [];
+			  function gtag(){dataLayer.push(arguments);}
+			  gtag('js', new Date());
+			  gtag('config', 'G-QZ651WN12Y');
+			`,
+		    }}
+		/>
 	    </Head>
 	    <Menu />
 	    <LandingPage />
 	    <CardWith4Images
-	    header={"Ośrodek Słoneczko"}
-	    title={"Drewniane domki niedaleko pięknej piaszczystej plaży."}
-	    desc={"Ośrodek Słoneczko-Domki położony jest zaledwie 350 m od piaszczystej plaży. Do dyspozycji gości oddajemy komfortowe, drewniane domki. Na gości czeka wiele atrakcji, w tym jacuzzi, sauna fińska, pomieszczenie gier i zabaw dla dzieci, wypożyczalnia rowerów, zewnętrzny plac zabaw oraz ogród pełen zieleni i kwiatów. Gościom oferujemy menu śniadaniowe oraz grillowe. Zapewniamy cichą i kameralną atmosferę w doskonałej lokalizacji w centrum Dźwirzyna, w izolacji od ruchu ulicznego i nocnego życia, w spacerowej odległości od plaży i wszelkich atrakcji miasteczka."}
-	    image11={"./domki.jpg"}
-	    image12={"./domek6.jpg"}
-	    image21={"./20230303_0667_przyciety.jpg"}
-	    image22={"./20230303_0884_przyciety.jpg"}
-	    elements={["jacuzzi i sauna", "śniadania", "grill", "sala zabaw", "wypożyczalnia rowerów", "duże tarasy", "plac zabaw dla dzieci", "teren ogrodzony i monitorowany", "parking"]}
+		header={"Ośrodek Słoneczko"}
+		title={"Drewniane domki niedaleko pięknej piaszczystej plaży."}
+		desc={"Ośrodek Słoneczko-Domki położony jest zaledwie 350 m od piaszczystej plaży. Do dyspozycji gości oddajemy komfortowe, drewniane domki. Na gości czeka wiele atrakcji, w tym jacuzzi, sauna fińska, pomieszczenie gier i zabaw dla dzieci, wypożyczalnia rowerów, zewnętrzny plac zabaw oraz ogród pełen zieleni i kwiatów. Gościom oferujemy menu śniadaniowe oraz grillowe. Zapewniamy cichą i kameralną atmosferę w doskonałej lokalizacji w centrum Dźwirzyna, w izolacji od ruchu ulicznego i nocnego życia, w spacerowej odległości od plaży i wszelkich atrakcji miasteczka."}
+		image11={"./domki.jpg"}
+		image12={"./domek6.jpg"}
+		image21={"./20230303_0667_przyciety.jpg"}
+		image22={"./20230303_0884_przyciety.jpg"}
+		elements={["jacuzzi i sauna", "śniadania", "grill", "sala zabaw", "wypożyczalnia rowerów", "duże tarasy", "plac zabaw dla dzieci", "teren ogrodzony i monitorowany", "parking"]}
 	    />
-		<Description />
+	    <Description />
 	    <OfferList />
-	    
-		<Carousel items={["./domki.jpg", "./domek6.jpg"]} />
+	    <Carousel items={["./domki.jpg", "./domek6.jpg"]} />
 	    <ContactSection />
 	</>
     );
 }
+
