bf3b8e0bce9a2560d0f7dc6746d6442898d02197
Łukasz Hen <lukasz.hen@gmail.com>
2025-05-14 13:35:16 +0200

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,15 +1,26 @@
 // routes/_app.tsx
 import { AppProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
   return (
-    <html lang="de">
-      <head>
-        <title>My Fresh App</title>
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
+    <>
+      <Head>
+        <title>Domki Słoneczko</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QZ651WN12Y"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QZ651WN12Y');
+            `,
+          }}
+        />
+      </Head>
+      <Component />
+    </>
   );
 }
